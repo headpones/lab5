2e8e9f05483bdc78f94bab101008485d0d80597b
headpones <krafergames228@gmail.com>
2024-12-12 21:26:25 +0200

    lab5

---
+++ Task2/Task2/Program.cs
@@ -0,0 +1,36 @@
+﻿class Program
+{
+    static void Main()
+    {
+        //task2
+        int size = 30;
+        int[] array = new int[size];
+        Random random = new Random();
+
+        for (int i = 0; i < size; i++)
+        {
+            array[i] = random.Next(-20, 21);
+        }
+
+        Console.WriteLine("Початковий масив:");
+        PrintArray(array);
+        int[] rearrangedArray = RearrangeArray(array);
+        Console.WriteLine("\nПеретворений масив:");
+        PrintArray(rearrangedArray);
+    }
+    static int[] RearrangeArray(int[] array)
+    {
+        var positive = array.Where(x => x > 0).OrderBy(x => x).ToArray();
+        var zeros = array.Where(x => x == 0).ToArray();
+        var negative = array.Where(x => x < 0).OrderBy(x => x).ToArray();
+        return positive.Concat(zeros).Concat(negative).ToArray();
+    }
+    static void PrintArray(int[] array)
+    {
+        foreach (var item in array)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ task1/task1/Program.cs
@@ -0,0 +1,63 @@
+﻿class Program
+{
+    static void Main()
+    {
+        // Розмірність матриці згідно з варіантом
+        int size = 7; // 7x7
+
+        // 1. Генерація матриці з випадковими елементами
+        int[,] matrix = new int[size, size];
+        Random random = new Random();
+
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                matrix[i, j] = random.Next(0, 101); // Елементи у діапазоні 0...100
+            }
+        }
+
+        // 2. Виведення початкової матриці
+        Console.WriteLine("Початкова матриця:");
+        PrintMatrix(matrix);
+
+        // 3. Відображення матриці симетрично відносно горизонтальної осі
+        SymmetrizeMatrixHorizontally(matrix);
+
+        // 4. Виведення перетвореної матриці
+        Console.WriteLine("\nПеретворена матриця:");
+        PrintMatrix(matrix);
+    }
+
+    // Метод для симетричного відображення матриці відносно горизонтальної осі
+    static void SymmetrizeMatrixHorizontally(int[,] matrix)
+    {
+        int size = matrix.GetLength(0);
+
+        for (int i = 0; i < size / 2; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                // Міняємо місцями елементи з верхнього та нижнього секторів
+                int temp = matrix[i, j];
+                matrix[i, j] = matrix[size - i - 1, j];
+                matrix[size - i - 1, j] = temp;
+            }
+        }
+    }
+
+    // Метод для виведення матриці у консоль
+    static void PrintMatrix(int[,] matrix)
+    {
+        int size = matrix.GetLength(0);
+
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                Console.Write($"{matrix[i, j],4}");
+            }
+            Console.WriteLine();
+        }
+    }
+}
